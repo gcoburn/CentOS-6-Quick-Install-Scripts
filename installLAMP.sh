efccc46855ac4c1b194ccd637d0897700589212d
gcoburn <gcoburn@vmware.com>
2016-01-18 12:36:04 -0500

    Update installLAMP.sh

--- installLAMP.sh
+++ installLAMP.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
+# $1 is the password you set in the command line argument
 
 clear
 
 echo 'Going to install the LAMP stack on your machine, here we go...'
 echo '------------------------'
-# $1 is the password you set in the command line argument
-
-#read -p "MySQL Password: " mysqlPassword
-#read -p "Retype password: " mysqlPasswordRetype
 
 yum install -y httpd php mysql mysql-server
 
@@ -20,8 +17,12 @@ echo restart MySQL
 echo Setting password from passed Argument
 /usr/bin/mysqladmin -u root password $1
 
+echo 'Starting Apache'
+service httpd start
+
+echo 'Stopping iptables'
+service iptables stop
+
 clear
 echo 'Okay.... apache, php and mysql is installed, running and set to your desired password'
 
-service httpd start
-service iptables stop
